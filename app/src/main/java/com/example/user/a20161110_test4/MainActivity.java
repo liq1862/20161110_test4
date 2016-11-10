76958b532c4135537ba31063f3e22363c3bb7579
lili <liq1862@gmail.com>
2016-11-10 15:41:20 +0800

    SimpleAdapter

--- app/src/main/java/com/example/user/a20161110_test4/MainActivity.java
+++ app/src/main/java/com/example/user/a20161110_test4/MainActivity.java
@@ -1,22 +1,48 @@
 package com.example.user.a20161110_test4;
 
+import android.support.v4.util.ArrayMap;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Adapter;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.SimpleAdapter;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
-    ListView lv;
     String[] fruits = {"蘋果", "鳳梨", "芭樂", "香蕉"};
-    ArrayAdapter<String > adapter;
+    String[] cities = {"台北", "台中", "台南", "高雄"};
+    String[] codes = {"02", "04", "06", "07"};
+    ArrayList<Map<String, String>> mylist;
+
+    ListView lv;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        setContentView(R.layout.activity_main);
         lv = (ListView) findViewById(R.id.listview);
 
-        adapter = new ArrayAdapter<String>(MainActivity.this,android.R.layout.simple_spinner_item,fruits);
+        // ArrayAdapter<String> adapter = new ArrayAdapter<String>(MainActivity.this,
+        //                     android.R.layout.simple_list_item_1, fruits);
+        mylist = new ArrayList<>();
+        for (int i=0;i<cities.length;i++)
+        {
+            Map m = new HashMap();
+            m.put("city", cities[i]);
+            m.put("code", codes[i]);
+            mylist.add(m);
+        }
+
+        SimpleAdapter adapter = new SimpleAdapter(MainActivity.this,
+                mylist, android.R.layout.simple_list_item_2,
+                new String[] {"city", "code"},
+                new int[] {android.R.id.text1, android.R.id.text2});
+
         lv.setAdapter(adapter);
     }
 }
